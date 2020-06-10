2ce412d5015d9cdeab966cb44606ec0cab20c080
Rodolfo Martins <rodolfovmartins@gmail.com>
2020-06-10 09:28:51 -0300

    run.py and corrections at config.py

--- config.py
+++ config.py
@@ -8,31 +8,31 @@ class Config(object):
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     APP = None
 
-    class DevelopmentConfig(Config):
-        TESTING = True
-        DEBUG = True
-        IP_HOST = 'localhost'
-        PORT_HOST = 8000
-        URL_MAIN = 'http://%s:$s' % (IP_HOST, PORT_HOST)
+class DevelopmentConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 8000
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
 
-    class TestingConfig(Config):
-        TESTING = True
-        DEBUG = True
-        IP_HOST = 'localhost'
-        PORT_HOST = 5000
-        URL_MAIN = 'http://%s:$s' % (IP_HOST, PORT_HOST)
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 5000
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
 
-    class ProductionConfig(Config):
-        TESTING = True
-        DEBUG = True
-        IP_HOST = 'localhost'
-        PORT_HOST = 8080
-        URL_MAIN = 'http://%s:$s' % (IP_HOST, PORT_HOST)
+class ProductionConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 8080
+    URL_MAIN = 'http://%s:%s' % (IP_HOST, PORT_HOST)
 
 app_config = {
-    'development': Config.DevelopmentConfig(),
-    'testing': Config.TestingConfig(),
-    'production': Config.ProductionConfig()
+    'development': DevelopmentConfig(),
+    'testing': TestingConfig(),
+    'production': ProductionConfig()
 }
 
 app_active = os.getenv('FLASK_ENV')
