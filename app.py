3d0acef06ab986bf8dfc03f71290ee838032dacb
Rodolfo Martins <rodolfovmartins@gmail.com>
2020-06-11 11:40:33 -0300

    Admin area created

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, redirect, render_template
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
 from controller.User import UserController
+from admin.Admin import start_views
+
 
 config = app_config[app_active]
 db = SQLAlchemy(config.APP)
@@ -14,7 +16,7 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
     app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
-
+    start_views(app, db)
     db.init_app(app)
 
     @app.route('/')
